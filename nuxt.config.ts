c4f2adba7330b273c21e5db4cb9bd2b6d577a9c7
imsobluu <11052192+imsobluu@users.noreply.github.com>
2025-05-09 20:08:37 +0700

    Set up ESLint and Code Formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+	modules: ["@nuxt/eslint"],
+	devtools: { enabled: true },
+	compatibilityDate: "2024-11-01",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+});
