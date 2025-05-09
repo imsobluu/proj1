f5efd22668c3b8f0a67aee9c66371572c42ea513
imsobluu <11052192+imsobluu@users.noreply.github.com>
2025-05-09 20:56:49 +0700

    Added nuxt/ui and nuxt/font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/eslint"],
+	modules: [
+		"@nuxt/eslint",
+		"@nuxt/ui",
+		"@nuxt/fonts",
+	],
 	devtools: { enabled: true },
+	css: ["@/assets/css/main.css"],
 	compatibilityDate: "2024-11-01",
 	eslint: {
 		config: {
