537087614964e273c682240e6656dc6a7ce70d0f
imsobluu <11052192+imsobluu@users.noreply.github.com>
2025-05-10 15:55:33 +0700

    Wireframe test page

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,11 +1,38 @@
 <template>
-	<div>
-		<shadcnButton
-			variant="default"
-			size="lg"
-			class="text-2xl bg-transparent border-2 border-white text-white"
-		>
-			Test
-		</shadcnButton>
+	<div class="h-full w-full flex flex-row">
+		<div class="flex flex-col flex-1">
+			<div class="flex items-center justify-between h-[60px] px-4">
+				<div class="flex flex-row gap-4">
+					<div class="">
+						Sidebar
+					</div>
+					<div class="">
+						Date
+					</div>
+					<div>
+						-
+					</div>
+					<div>
+						Time
+					</div>
+				</div>
+				<div class="flex flex-row gap-4">
+					<div>
+						Close Order
+					</div>
+				</div>
+			</div>
+			<div class="flex flex-1 justify-center items-center">
+				Content
+			</div>
+		</div>
+		<div class="flex flex-col w-1/3">
+			<div class="flex h-[60px] justify-center items-center">
+				Padding
+			</div>
+			<div class="flex flex-1 justify-center items-center">
+				Details
+			</div>
+		</div>
 	</div>
 </template>
